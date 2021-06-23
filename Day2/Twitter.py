a1cb67d0bb154828e60cc91f2b1ef9586face127
Kahenyaa <kahenyaa@gmail.com>
2021-06-23 15:18:20 +0300

    Getting Number of Twitter Followers

---
+++ Day2/Twitter.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+
+handle = input('input your handle here:')
+driver = webdriver.Chrome()
+driver.implicitly_wait(600)
+driver.get('http://twitter.com/'+handle)
+html = driver.find_element_by_xpath('//a[@href="/'+handle+'/followers"]')
+print (html.text) 
