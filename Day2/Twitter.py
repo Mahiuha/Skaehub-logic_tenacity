9fc7a25a2e1ec63313a6f34312769006d0367a63
sarah njeri <60597568+Sarah358@users.noreply.github.com>
2021-06-23 16:06:35 +0300

    try catch update

--- Day2/Twitter.py
+++ Day2/Twitter.py
@@ -5,5 +5,8 @@ handle = input('input your handle here:')
 driver = webdriver.Chrome()
 driver.implicitly_wait(600)
 driver.get('http://twitter.com/'+handle)
-html = driver.find_element_by_xpath('//a[@href="/'+handle+'/followers"]')
-print (html.text) 
+try:
+  html = driver.find_element_by_xpath('//a[@href="/'+handle+'/followers"]')
+  print (html.text)
+except:
+  print('Acount not found')
